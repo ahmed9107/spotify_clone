b924b68e30743aa8edb3629c8647d95952e597df
unknown <ahmedlemarshall@gmail.com>
2022-07-04 17:51:52 +0100

    refresh

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:spotify_clone/view/pages/layout.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Layout(),
   ));
